9a4e1bda28f2a0f6f8f24f9ab2ace0f4d7b7634a
Gabriel Lavarini <gabriel@lavarinimoreira.me>
2023-06-26 19:54:33 -0300

    updating projects

--- src/components/english/projects/index.tsx
+++ src/components/english/projects/index.tsx
@@ -1,17 +1,32 @@
-import { ProjectsCard, ProjectsContainer, ProjectsGrid } from '../../styles/projects';
+import { LineBreak, ProjectsCard, ProjectsContainer, ProjectsGrid } from '../../styles/projects';
 import projectsEnglish from '../../../data/projects.json';
 
 function EnglishProjects() {
     return (
         <ProjectsContainer>
             <h1>Projects</h1>
-            <span>Tip: You can click on a project to see more details</span>
+            <LineBreak />
             <ProjectsGrid>
                 {projectsEnglish.projectsEnglish.map((project) => (
                     <ProjectsCard key={project.id}>
                         <h2>{project.title}</h2>
                         <img src={project.image} />
                         <p>{project.date}</p>
+                        <p>{project.description}</p>
+                        <h3>Links:</h3>
+                        <ul>
+                            <li>
+                                <a href={project.web} target='_blank' rel='noreferrer'>
+                                    Site - {project.webTitle}
+                                </a>
+                            </li>
+                            <li>
+                                <a href={project.git} target='_blank' rel='noreferrer'>
+                                    GitHub - {project.id}
+                                </a>
+                            </li>
+                        </ul>
+                        <LineBreak />
                     </ProjectsCard>
                 ))}
             </ProjectsGrid>
--- src/components/styles/projects.ts
+++ src/components/styles/projects.ts
@@ -16,11 +16,38 @@ export const ProjectsContainer = styled.div`
     padding: 4rem 6rem;
     border-radius: 48px;
     background-color: ${({ theme }) => theme.colors.contentBg};
+
+    h1 {
+        margin: 0 0 0.5rem 0;
+        font-size: ${({ theme }) => theme.fontSize['4xl']};
+    }
+
+    h2 {
+        margin: 3rem 0 1rem 0;
+    }
+
+    h3 {
+        margin: 1rem 0 0 0;
+    }
+
+    a {
+        text-decoration: none;
+        line-height: 1.5rem;
+        font-weight: bold;
+        color: ${({ theme }) => theme.colors.pink};
+    }
+
+    p {
+        margin: 0;
+        line-height: 1.5rem;
+        text-align: justify;
+        color: ${({ theme }) => theme.colors.gray400};
+    }
 `;
 
 export const ProjectsGrid = styled.div`
     display: grid;
-    grid-gap: 40px;
+    grid-gap: 0;
     grid-template-columns: repeat(1, 1fr);
 `;
 
@@ -29,15 +56,17 @@ export const ProjectsCard = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-
-    background-color: rgba(55, 48, 163, 0.2);
-    border: 8px solid black;
-    border-radius: 24px;
-
     padding: 0 2rem;
 
     img {
         width: 600px;
         height: 310px;
     }
+`;
+
+export const LineBreak = styled.div`
+    width: 100%;
+    border-top: ${({ theme }) => theme.colors.gray500} solid 1px;
+    height: 1px;
+    margin-top: 1rem;
 `;
